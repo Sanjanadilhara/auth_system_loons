aa00c2c6152e8f7d7b4210a12dd6ed54b5d4a6c5
sanjanadilhara <sanjanadilhara2000@gmail.com>
2024-02-10 18:36:19 +0530

    final

--- test.js
+++
@@ -1,8 +0,0 @@
-const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-
-const email = 'use r@example.com';
-if (emailRegex.test(email)) {
-  console.log('Valid email address');
-} else {
-  console.log('Invalid email address');
-}
